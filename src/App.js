0dfe56f08a3a0e14fffaed75c86387684874367c
Preston Lund <49794144+prestonlund@users.noreply.github.com>
2020-05-06 16:40:58 -0600

    import Calculator component to App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
 import "./styles.css";
+import Calculator from "./components/Calculator";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Calculator</h1>
-      <button>0</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>.</button>
-      <button>+</button>
-      <button>-</button>
-      <button>*</button>
-      <button>/</button>
-      <button>=</button>
-      <button>C</button>
+      <Calculator />
     </div>
   );
 }
