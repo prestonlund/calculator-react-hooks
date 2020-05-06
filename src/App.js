508fc397aca76fae673339b718da7efbc8b222f6
Preston Lund <49794144+prestonlund@users.noreply.github.com>
2020-05-06 16:28:45 -0600

    create Calculator component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Let's make a calculator</h1>
-      <h2>Here we goooo!</h2>
+      <h1>Calculator</h1>
+      <button>0</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>.</button>
+      <button>+</button>
+      <button>-</button>
+      <button>*</button>
+      <button>/</button>
+      <button>=</button>
+      <button>C</button>
     </div>
   );
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Calculator = () => (
+  <div className="display">
+    <h1>Calculator</h1>
+  </div>
+  <div className='number-pad'>
+    <button>0</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>.</button>
+    <button>+</button>
+    <button>-</button>
+    <button>*</button>
+    <button>/</button>
+    <button>=</button>
+    <button>C</button>
+    <button>+-</button>
+  </div>
+);
