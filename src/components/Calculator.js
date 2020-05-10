9b50af13b2f0e6108c202179f1eb0ab9657536c5
Preston Lund <49794144+prestonlund@users.noreply.github.com>
2020-05-09 22:03:56 -0600

    change button order

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,16 +7,16 @@ const Calculator = () => (
       <h1>Calculator</h1>
     </div>
     <div className="number-pad">
-      <NumberButton buttonValue={0} />
-      <NumberButton buttonValue={1} />
-      <NumberButton buttonValue={2} />
-      <NumberButton buttonValue={3} />
-      <NumberButton buttonValue={4} />
-      <NumberButton buttonValue={5} />
-      <NumberButton buttonValue={6} />
-      <NumberButton buttonValue={7} />
-      <NumberButton buttonValue={8} />
       <NumberButton buttonValue={9} />
+      <NumberButton buttonValue={8} />
+      <NumberButton buttonValue={7} />
+      <NumberButton buttonValue={6} />
+      <NumberButton buttonValue={5} />
+      <NumberButton buttonValue={4} />
+      <NumberButton buttonValue={3} />
+      <NumberButton buttonValue={2} />
+      <NumberButton buttonValue={1} />
+      <NumberButton buttonValue={0} />
       <button>.</button>
       <button>+</button>
       <button>-</button>
