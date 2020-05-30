91f752e4a9aa2cb84d7a2b8109b80fbb57f7bbaf
Preston Lund <49794144+prestonlund@users.noreply.github.com>
2020-05-30 10:40:32 -0600

    make switch case statement to change division and multiplication symbols

--- src/components/FunctionButton.js
+++ src/components/FunctionButton.js
@@ -3,13 +3,24 @@ import { NumberContext } from "./NumberProvider";
 
 const FunctionButton = ({ buttonValue }) => {
   const { handleSetCalcFunction } = useContext(NumberContext);
+
   return (
     <button
       className="function-button"
       type="button"
       onClick={() => handleSetCalcFunction(buttonValue)}
     >
-      {buttonValue}
+      {/* {buttonValue} === '*' ? 'X' : {buttonValue} */}
+      {(() => {
+        switch (buttonValue) {
+          case "*":
+            return "x";
+          case "/":
+            return "÷";
+          default:
+            return buttonValue;
+        }
+      })()}
     </button>
   );
 };
